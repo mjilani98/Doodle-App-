8b88f09c956dcb0b6a8b3d43a440f7e69796007c
mjilani98 <mjilani@emich.edu>
2025-03-31 15:14:33 -0400

    Created DoodleModel.java

--- app/src/main/java/com/example/hw4q3/DoodleModel.java
+++ app/src/main/java/com/example/hw4q3/DoodleModel.java
@@ -1,4 +1,48 @@
 package com.example.hw4q3;
 
-public class DoodleModel {
+import java.util.LinkedList;
+
+public class DoodleModel
+{
+    private int color ;
+
+    //Linked list of points
+    private LinkedList<Point> list;
+
+    //public constructor
+    public DoodleModel()
+    {
+        color = 0 ;
+        list = new LinkedList<>();
+    }
+
+    //method points to a color
+    public void nextColor()
+    {
+        color = (color + 1) & 8;
+    }
+
+    //method adds a point to the list of point
+    public void addPoint(int x , int y )
+    {
+        //create a point using x , y , color
+        Point point  = new Point(x,y,color);
+
+        //add the point to the list
+        list.add(point);
+    }
+
+    //method returns the color
+    public int getColor()
+    {
+        return color;
+    }
+
+    //method returns the listv
+    public LinkedList getList()
+    {
+        return list;
+    }
+
+
 }
