895bef83b1ea2d2e14a5d3ef1703fe727309dfd1
mjilani98 <mjilani@emich.edu>
2025-03-31 14:58:54 -0400

    Added x,y,color , Point.java

--- app/src/main/java/com/example/hw4q3/Point.java
+++ app/src/main/java/com/example/hw4q3/Point.java
@@ -1,4 +1,16 @@
 package com.example.hw4q3;
 
 public class Point {
+
+    public float x , y;
+    public int color;
+
+    //public constructor
+    public Point(float x , float y , int color)
+    {
+        this.x = x;
+        this.y = y;
+        this.color = color; 
+    }
+
 }
